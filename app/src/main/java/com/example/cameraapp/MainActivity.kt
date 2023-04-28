e3c3a8ed1b1a6cb8f2dc3de6b903e3aa5219a4de
brendan.craven <brendan.craven@student.howest.be>
2023-04-29 00:57:36 +0200

    a new MainActivity file and a quick test to see if kotlin compose renders properly

---
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.cameraapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cameraapp.ui.theme.CameraAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CameraAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    CameraPreviewContent(Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CameraPreviewContent(modifier : Modifier = Modifier) {
+    Text("hey")
+}
