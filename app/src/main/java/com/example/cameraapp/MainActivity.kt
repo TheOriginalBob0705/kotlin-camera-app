42e3e51f1850de810776437d27ea9768bb1c18d9
brendan.craven <brendan.craven@student.howest.be>
2023-04-29 01:21:02 +0200

    ask for permissions in the app on startup

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.cameraapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cameraapp.ui.theme.CameraAppTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
 
+@ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +30,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@ExperimentalPermissionsApi
 @Composable
 fun CameraPreviewContent(modifier : Modifier = Modifier) {
-    Text("hey")
+    Permission(
+        permission = android.Manifest.permission.CAMERA,
+        reason = "This app requires permission to use the camera in order to work properly",
+        permissionDeniedOutput = {
+            Column(modifier) {
+                Text("Permission denied")
+                Spacer(modifier = Modifier.height(12.dp))
+                Button(onClick = { /* TODO */ }) {
+                }
+            }
+        }
+    ) {
+        Text("Permission granted")
+    }
+}
+
+@ExperimentalPermissionsApi
+@Composable
+fun Permission(
+    permission : String = android.Manifest.permission.CAMERA,
+    reason : String = "Camera permissions are needed for the app to work",
+    permissionDeniedOutput : @Composable () -> Unit = {},
+    output : @Composable () -> Unit = {}
+) {
+    val permissionState = rememberPermissionState(permission)
+    PermissionRequired(
+        permissionState = permissionState,
+        permissionNotGrantedContent = {
+            Reason(
+              message = reason,
+              onRequestPermission = { permissionState.launchPermissionRequest() }
+            )
+        },
+        permissionNotAvailableContent = permissionDeniedOutput,
+        content = output
+    )
+}
+
+@Composable
+private fun Reason(
+    message : String,
+    onRequestPermission : () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { /*NONE*/ },
+        title = {
+            Text(text = "Permission request")
+        },
+        text = {
+            Text(message)
+        },
+        confirmButton = {
+            Button(onClick = onRequestPermission) {
+                Text("Ok")
+            }
+        }
+    )
 }
