3d9ba77db9997af4ed5e1cbdff45bd817b4a449e
brendan.craven <brendan.craven@student.howest.be>
2023-04-29 13:42:41 +0200

    show camera preview on screen

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.cameraapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.example.cameraapp.ui.theme.CameraAppTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.rememberPermissionState
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.launch
 
 @ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
@@ -35,17 +50,17 @@ class MainActivity : ComponentActivity() {
 fun CameraPreviewContent(modifier : Modifier = Modifier) {
     Permission(
         permission = android.Manifest.permission.CAMERA,
-        reason = "This app requires permission to use the camera in order to work properly",
+        reason = "This app requires permission to use the camera",
         permissionDeniedOutput = {
             Column(modifier) {
                 Text("Permission denied")
                 Spacer(modifier = Modifier.height(12.dp))
-                Button(onClick = { /* TODO */ }) {
+                Button(onClick = { /* TODO make a button to take you to settings and set the permission */ }) {
                 }
             }
         }
     ) {
-        Text("Permission granted")
+        CameraPreview(modifier)
     }
 }
 
@@ -91,3 +106,56 @@ private fun Reason(
         }
     )
 }
+
+@Composable
+fun CameraPreview(
+    modifier : Modifier = Modifier,
+    camScale : PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+    camSelector : CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+) {
+    val corScope = rememberCoroutineScope()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = camScale
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+
+            val previewUseCase = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            corScope.launch {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    cameraProvider.unbindAll()
+
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, camSelector, previewUseCase
+                    )
+                } catch (ex : Exception) {
+                    Log.e("fun: CameraPreview", "Couldn't bind use case", ex)
+                }
+            }
+            previewView
+        }
+    )
+}
+
+suspend fun Context.getCameraProvider() : ProcessCameraProvider = suspendCoroutine { continuationProvider ->
+    ProcessCameraProvider.getInstance(this).also { future ->
+        future.addListener({
+            continuationProvider.resume(future.get())
+        }, executor)
+    }
+}
+
+val Context.executor : Executor
+    get() = ContextCompat.getMainExecutor(this)
