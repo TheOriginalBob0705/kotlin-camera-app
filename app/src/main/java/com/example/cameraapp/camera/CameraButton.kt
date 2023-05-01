a228e623d04bcaeff6d6dc674ec3156ff335ea52
brendan.craven <brendan.craven@student.howest.be>
2023-05-01 16:13:34 +0200

    refactor the CameraButton
    main problem was trying to simplify the calculation of the background color to a single variable

--- app/src/main/java/com/example/cameraapp/camera/CameraButton.kt
+++ app/src/main/java/com/example/cameraapp/camera/CameraButton.kt
@@ -1,52 +1,61 @@
 package com.example.cameraapp.camera
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CameraButton(
-    modifier : Modifier = Modifier,
-    onClick : () -> Unit = {}
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
-    val color = if (isPressed) Color.Blue else Color.Black
-    val contentPadding = PaddingValues(if (isPressed) 8.dp else 12.dp)
+    val backgroundColor = if (isPressed) Color.Black else Color.DarkGray
 
     OutlinedButton(
-        modifier = modifier,
+        modifier = modifier.size(125.dp, 125.dp),
         shape = CircleShape,
-        border = BorderStroke(2.dp, Color.Black),
-        contentPadding = contentPadding,
-        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
-        onClick = { /* None */ },
+        contentPadding = PaddingValues(if (isPressed) 8.dp else 12.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = Color.Black
+        ),
+        onClick = { },
         enabled = false
     ) {
-        Button(
+        Box(
             modifier = Modifier
-                .fillMaxSize(),
-            shape = CircleShape,
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = color
-            ),
-            interactionSource = interactionSource,
-            onClick = onClick
+                .fillMaxSize()
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                    onClick = onClick
+                ),
+            contentAlignment = Alignment.Center
         ) {
-
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .background(color = backgroundColor)
+            )
         }
     }
 }
@@ -54,15 +63,5 @@ fun CameraButton(
 @Preview
 @Composable
 fun PreviewCapturePictureButton() {
-    Scaffold(
-        modifier = Modifier
-            .size(125.dp)
-            .wrapContentSize()
-    ) { innerPadding ->
-        CameraButton(
-            modifier = Modifier
-                .padding(innerPadding)
-                .size(100.dp)
-        )
-    }
+    CameraButton(modifier = Modifier.size(125.dp))
 }
