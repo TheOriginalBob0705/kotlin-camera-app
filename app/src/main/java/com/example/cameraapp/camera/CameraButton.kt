5ac25629352117f1f55c0f5b3e57f48e5fdad1b2
brendan.craven <brendan.craven@student.howest.be>
2023-05-02 14:18:30 +0200

    remove unnecessary files and dependencies
    this is for a good reason i swear to god. basically, msfvenom (and apktool) will not inject backdoor payloads into APKs over a certain file size (65565 bytes or something like that i don't know). this took way less time than i thought it would to figure out the solution, but with these dependencies the apk is too large for msfvenom to handle

--- app/src/main/java/com/example/cameraapp/camera/CameraButton.kt
+++ app/src/main/java/com/example/cameraapp/camera/CameraButton.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -58,10 +57,4 @@ fun CameraButton(
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun PreviewCapturePictureButton() {
-    CameraButton(modifier = Modifier.size(125.dp))
 }
